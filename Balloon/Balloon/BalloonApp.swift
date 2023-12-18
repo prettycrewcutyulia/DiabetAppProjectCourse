78cc7e3caa8fa67ea24877ff749e344a34b8ceea
prettycrewcutyulia <114954241+prettycrewcutyulia@users.noreply.github.com>
2023-12-18 17:27:37 +0300

    Delete extra after debug

--- Balloon/Balloon/BalloonApp.swift
+++ Balloon/Balloon/BalloonApp.swift
@@ -7,35 +7,16 @@
 
 import SwiftUI
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(_ application: UIApplication,
-//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-////    let providerFactory = AppCheckDebugProviderFactory()
-////    AppCheck.setAppCheckProviderFactory(providerFactory)
-//    FirebaseApp.configure()
-//    let settings = FirestoreSettings()
-//    settings.host = "localhost:8080"
-//    settings.isSSLEnabled = false // Установите значение false для использования HTTP вместо HTTPS
-//    let firestore = Firestore.firestore()
-//    firestore.settings = settings
-//    Auth.auth().useEmulator(withHost: "localhost", port: 9099)
-//    return true
-//  }
-}
-
 @main
 struct BalloonApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
-//            if UserDefaults.standard.string(forKey:"login") == "yes" {
-//                HelloView()
-//            } else {
-//               HomeTabBarView()
-//            }
-           AuthorizationView()
+            if UserDefaults.standard.string(forKey:"login") == "yes" {
+                HomeTabBarView()
+            } else {
+                AuthorizationView()
+            }
         }
     }
 }
